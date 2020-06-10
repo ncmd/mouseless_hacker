f7cd4b36da4242411634908585b55821238652f4
CavalryTactics <noreply@cavalrytactics.com>
2020-06-09 23:12:57 -0700

    Updated configuration

--- install.sh
+++ install.sh
@@ -12,37 +12,64 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 echo ${machine}
-if [ ${machine} == "Mac"];
-    # Plugin Manager
+if [ ${machine} == "Mac"]; then
+    # === HOMEBREW ===
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+    # === KARABINER ===
+    # non-us backslash = backslash
+    # right_command = right control
+    brew install karabiner-elements
+    cp ./karabiner_complex_modifications_kinesis.json ~/.config/karabiner/assets/complex_modifications/mods.json
+
+    # === NEOVIM ===
+    # VIM Plugin Manager
+    brew install neovim
+    brew install python3
+    brew tap homebrew/cask-fonts
+    brew cask install font-fira-code
+    cp ./.vimrc ~/.config/nvim/init.vim
+    cp ./coc-settings.json ~/.config/nvim/coc-settings.json
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-    # BREW
-    brew install neovim
-    brew install python3
     pip install pynvim
     pip3 install pynvim
     npm install -g neovim
+    nvim -c PlugInstall
+    nvim -c ":PlugInstall"
+    nvim -c ":source ~/.vimrc"
 
     # === RUST ===
-    stable rust
-    rustup
-    cargo
-    cargo-make
-    # pier
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup component add rls rust-analysis rust-src
+    rustup toolchain add nightly
+    rustup toolchain add stable
+    cargo install --force cargo-make
     cargo install pier
+    # cargo install 
 
-
-    # fzf
+    # === FZF ===
     brew install fzf
     $(brew --prefix)/opt/fzf/install
-    :
 
-# TMUX
+    # === TMUX ===
+    brew install tmux
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    cp ./.tmux.conf ~/.tmux.conf
+    cp ./.tmux.conf.local ~/.tmux.conf.local
+    tmux source-file ~/.tmux.conf
 
+    # === ITERM2 ===
+    brew install iterm2
+    mkdir ~/iterm2
+    cp ./iterm2.plist ~/iterm2/iterm2.plist
 
-# VIM
+    # === AMETHYST ===
+    brew install amethyst
 
-
+    # === ZSH ===
+    cp ./.zshrc ~/.zshrc
+    source zshrc
+fi
